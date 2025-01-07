197323e35185678f4e3589504d5a9ec6f1a42a1b
Son Juwan <wndhks171@naver.com>
2025-01-07 14:03:51 +0900

    signin logic

---
+++ Demopractice6/app/src/main/java/com/example/demo_practice/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.demo_practice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // 먼저 레이아웃을 설정
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view) // 이제 뷰를 찾을 수 있습니다
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
